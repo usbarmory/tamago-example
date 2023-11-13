acc714b6f9cff439e0764d5d796a49381f673404
Andrea Barisani <andrea@inversepath.com>
2023-11-13 14:27:50 +0100

    fix riscv64 semihosting exit sequence

--- internal/semihosting/exit_riscv64.s
+++ internal/semihosting/exit_riscv64.s
@@ -6,8 +6,11 @@
 
 // func sys_exit()
 TEXT ·sys_exit(SB), $0
-	MOV	$0x18,    A0
-	MOV	$0x20026, A1
+	MOV	$0x18,    A0 // TARGET_SYS_EXIT
+	MOV	$0x20026, T1 // ADP_Stopped_ApplicationExit
+
+	MOV	-2*8(SP), A1
+	MOV	T1, (A1)
 
 	SLLI	$0x1f, X0, X0
 	EBREAK
