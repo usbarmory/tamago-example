39910cec5e07b73851d0e412c2a0850365fea0f6
Andrea Barisani <andrea@inversepath.com>
2023-11-13 14:27:14 +0100

    tidying

--- internal/semihosting/exit_arm.s
+++ internal/semihosting/exit_arm.s
@@ -6,7 +6,7 @@
 
 // func sys_exit()
 TEXT ·sys_exit(SB), $0
-	MOVW	$0x18,    R0
-	MOVW	$0x20026, R1
+	MOVW	$0x18,    R0	// TARGET_SYS_EXIT
+	MOVW	$0x20026, R1	// ADP_Stopped_ApplicationExit
 	WORD	$0xef123456	// svc 0x00123456
 	RET
