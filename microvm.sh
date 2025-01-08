7d46595dd13905f3e5410b39a26a43c03df21250
Andrea Barisani <andrea@inversepath.com>
2025-01-08 13:41:53 +0100

    automatic entry point note

--- microvm.sh
+++ microvm.sh
@@ -1,10 +1,12 @@
 make clean && TARGET=microvm make example
 
+DESC=$(readelf -h example | grep "Entry point address"|grep -o -E "[a-f0-9]{8}$"|sed -e 's/\(..\)\(..\)\(..\)\(..\)/\\x\4\\x\3\\x\2\\x\1/')
+
 echo -e -n "\x04\x00\x00\x00" >  note.bin # namesz
 echo -e -n "\x04\x00\x00\x00" >> note.bin # descsz
 echo -e -n "\x12\x00\x00\x00" >> note.bin # type
 echo -e -n "Go \x00"          >> note.bin # name
-echo -e -n "\x80\xd4\x07\x10" >> note.bin # desc (** change me to Entry point address **)
+echo -e -n "$DESC"            >> note.bin # desc (** change me to Entry point address **)
 
 #objcopy --add-section .note.phv_start_addr=note.bin example
 objcopy --update-section .note.go.buildid=note.bin example
@@ -18,7 +20,7 @@ if [ "$1" == "gdb" ]; then
 fi
 
 qemu-system-x86_64 \
-  -machine microvm -enable-kvm -cpu host -no-reboot \
+  -machine microvm -enable-kvm -cpu host,invtsc=on -no-reboot \
   -m 1G -nographic -monitor none -serial stdio -kernel example $OPTS \
   -device virtio-net-device,netdev=net0 -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
   # -net none
