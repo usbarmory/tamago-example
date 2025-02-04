8cc7416aff15594a1b6af2e2ed8b420bf92586a2
Andrea Barisani <andrea@inversepath.com>
2025-02-04 12:00:15 +0100

    enable network tests for amd64

--- cmd/dns.go
+++ cmd/dns.go
@@ -3,7 +3,7 @@
 // Use of this source code is governed by the license
 // that can be found in the LICENSE file.
 
-//go:build mx6ullevk || usbarmory
+//go:build mx6ullevk || usbarmory || microvm || firecracker
 
 package cmd
 
